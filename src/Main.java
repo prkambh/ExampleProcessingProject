f3fd8103f52c18f8ec2860f263f38bc9476d00a2
Pallavi Kambham <pallavi_kambham@pallavis-macbook-pro.local>
2023-01-11 17:50:28 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import processing.core.PApplet;
+
+public class Main extends PApplet{
+
+    public static void main(String[] args) {
+        PApplet.main("Main");
+    }
+
+    public void settings(){
+        size(600, 600);
+    }
+
+    public void setup(){}
+
+    public void draw(){
+        rect(50,50,100,100);
+    }
+}
